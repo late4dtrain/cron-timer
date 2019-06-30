b4ce0a5f6578fe54e82aa9fba9d4b65920583513
Gab Soulavy <g.soulavy@gmail.com>
2019-06-30 21:25:18 +0100

    add initial e2e test for refactoring

---
+++ Late4Train.CronTimer.Tests/CronPhoenixTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Late4Train.CronTimer.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using Cronos;
+    using FluentAssertions;
+    using FluentAssertions.Execution;
+    using Xunit;
+
+    public class CronPhoenixTests
+    {
+        public static IEnumerable<object[]> GetCronFacts()
+        {
+            yield return new object[]
+            {
+                "* * * * * *", CronFormat.IncludeSeconds, new DateTime(2019, 06, 29, 12, 00, 01, DateTimeKind.Utc),
+                new DateTime(2019, 06, 29, 12, 00, 02, DateTimeKind.Utc)
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetCronFacts))]
+        public void Original(string expression, CronFormat format, DateTime testDate, DateTime? nextDate)
+        {
+            var cronExpression = CronExpression.Parse(expression, format);
+
+            var next = cronExpression.GetNextOccurrence(testDate);
+
+            using (new AssertionScope())
+            {
+                next.Should().NotBeNull();
+                next.Should().Be(nextDate);
+            }
+        }
+    }
+}
