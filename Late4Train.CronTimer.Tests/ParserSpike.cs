4e697074c53d080f7be502e5ed28816d1aaf88d0
Gab Soulavy <g.soulavy@gmail.com>
2020-11-13 13:05:59 +0000

    start spike

---
+++ Late4Train.CronTimer.Tests/ParserSpike.cs
@@ -0,0 +1,41 @@
+﻿namespace Late4Train.CronTimer.Tests
+{
+    using System.Text.RegularExpressions;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ParserSpike
+    {
+        [Theory]
+        [InlineData("* * * * *", "Every minute")]
+        [InlineData("5 * * * *", "At minute 5")]
+        [InlineData("6 * * * *", "At minute 6")]
+        [InlineData("1 * * * *", "At minute 1")]
+        [InlineData("59 * * * *", "At minute 59")]
+        [InlineData("60 * * * *", "Didn't match at all")]
+        [InlineData("600 * * * *", "Didn't match at all")]
+        [InlineData("a * * * *", "Didn't match at all")]
+        [InlineData("5-10 * * * *", "At every minute from 5 through 10")]
+        public void ParseMinuteSpikeTest(string input, string expected)
+        {
+            var schedule = input.Split(' ');
+            var minute = schedule[0];
+            var result = ParseMinute(minute);
+            result.Should().Be(expected);
+        }
+
+        public string ParseMinute(string schedule) =>
+            schedule switch
+            {
+                var s when R(s, @"[6][0-9]", out var _) => "Didn't match at all",
+                var s when R(s, @"[1-5]?[0-9]", out var atMinute) => $"At minute {atMinute.Captures[0].Value}",
+                var s when R(s, @"[*]", out var _) => "Every minute",
+                _ => "Didn't match at all"
+            };
+
+        private bool R(string input, string pattern, out Match match)
+            => (match = Regex.Match(input, pattern)).Success;
+    }
+    
+    
+}
